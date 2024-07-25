a9c438359d5f136a95eb90779620d318d5425a54
Anton Babych <117450388+AntonBabychP1T@users.noreply.github.com>
2024-07-25 08:22:33 +0300

    Add project files.

---
+++ PowerOutageSchedule/Models/OutageSchedule.cs
@@ -0,0 +1,15 @@
+﻿namespace PowerOutageSchedule.Models
+{
+    public class OutageSchedule
+    {
+        public int GroupNumber { get; set; }
+        public List<TimeInterval> OutageIntervals { get; set; }
+    }
+
+    public class TimeInterval
+    {
+        public TimeSpan Start { get; set; }
+        public TimeSpan End { get; set; }
+    }
+
+}
